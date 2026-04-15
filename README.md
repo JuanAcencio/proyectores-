5cf3d659a43350883b6629f5952b0cdc38ce03c1
Juan Manuel Acencio Chan <23470126@campeche.tecnm.mx>
2026-04-15 08:36:57 -0600

    Se modifico el titulo del archivo README

---
+++ README.md
@@ -0,0 +1,4 @@
+**#Proyectores#**
+Proyectores es una aplicacion web de ASP.NET
+gestionara las reservaciones y prestamos de proyectores a los docentes.
+
---
+++ WebApp/Controllers/ProyectoresController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProyectoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content(
+                "<p>Proyectores</p>" +
+                "<p> Aqui se mostrara la informacion de un proyector especifico</p>",
+                "text/html"
+            );
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Content(
+                "<h1>Nuevo Proyector </h1>" +
+                "<p> Aqui se mostrara la informacion del proyector  #" + id + "</p>",
+                "text/html"
+            );
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            return Content(
+                "<h1>Dar de baja </h1>" +
+                "<p> Aqui se dara de baja el proyector # " + id + "</p>",
+                "text/html"
+            );
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content(
+                "<h1>Editar </h1>" +
+                "<p> Aqui se modificara la informacion del proyector # " + id + "</p>",
+                "text/html"
+            );
+        }
+
+
+
+    }
+
+}
+
