68c18252e95b95b1e1e0b90ef8e03c3ce2dc89dd
Juan Manuel Acencio Chan <23470126@campeche.tecnm.mx>
2026-05-08 11:22:57 -0600

    Realiza validadiones usando DataAnnotations

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -31,8 +31,16 @@ namespace WebApp.Controllers
         [HttpPost]
         public IActionResult Create(Proyector proyector)
         {
-            _service.AddProyector(proyector);
-            return RedirectToAction(nameof(Index));
+            if (!ModelState .IsValid)
+            {
+                return View(proyector);
+            }
+            else 
+            {
+                _service.AddProyector(proyector);
+                return RedirectToAction(nameof(Index));
+            }
+          
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- WebApp/Models/Proyector.cs
+++ WebApp/Models/Proyector.cs
@@ -1,13 +1,21 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Proyector
     {
         public int Id { get; set; }
+        [Required (ErrorMessage ="Debe proporcionar un valor para el Marca")]
+        [StringLength(24)]
         public string Marca { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar un valor para el Modelo")]
         public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar un valor para el Numero de Serie")]
         public string NumeroDeSerie { get; set; }
-        public SituacionProyector Situacion  { get; set; }
-        public DateTime FechaDeAlta { get; set; }
+        public SituacionProyector Situacion { get; set; } = SituacionProyector.Bueno;
+        public DateTime FechaDeAlta { get; set; } = DateTime.Now;
         public DateTime? FechDeBaja { get; set; }
 
 
